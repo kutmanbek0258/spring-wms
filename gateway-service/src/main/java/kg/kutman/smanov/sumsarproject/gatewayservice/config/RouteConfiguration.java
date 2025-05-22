0d41bf851a367a0de101ac0aaeb522f64d9e392b
Kutman Smanov <smanovkutman0258@gmail.com>
2025-05-22 14:14:58 +0600

    Check point 5

--- gateway-service/src/main/java/kg/kutman/smanov/sumsarproject/gatewayservice/config/RouteConfiguration.java
+++ gateway-service/src/main/java/kg/kutman/smanov/sumsarproject/gatewayservice/config/RouteConfiguration.java
@@ -1,9 +1,7 @@
 package kg.kutman.smanov.sumsarproject.gatewayservice.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RouteConfiguration {
-
 }
